f34728d755ab21072ff6fc1fd4d4785c38dbcef8
Marcglacier32 <mark.thiongo@student.moringaschool.com>
2025-04-10 19:34:36 +0300

    Added __init__ to Dog with name and optional breed, and to Person with name

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+    
+    def bark(self):  # Keeping from previous assignment
+        print("Woof!")
+    
+    def sit(self):   # Keeping from previous assignment
+        print("The dog is sitting.")
+        
